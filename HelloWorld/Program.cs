f7dfecfac46f73a4d5fb81d038ad9c226389ebec
Administrator <Administrator@PC0J0U97>
2024-02-06 18:16:48 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.ComnampilerServices;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    //PascalClase
+
+   public class Program
+    { 
+
+       public static void Main(string[] args)
+       {
+            //camelCase
+            string firstAndlastName = "Valentina";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nazywam sie Valentina Gaidarji.");
+            if (PI_NUMBER > 2)
+            {
+            }
+
+            string WritePINumber(double pi)
+            
+                return ("3.14");
+
+                private string WritePINumber(double pi)
+                {
+                    return ("3.14");
+                }
+
+            
+        }
+    }
+}
+
